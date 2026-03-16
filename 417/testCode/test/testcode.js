594b6b40728a6928899bda9c16ddb23be0015a7d
Erroll B <94317465+Jr24Future@users.noreply.github.com>
2026-03-16 23:43:44 +0000

    adding class SE417

--- 417/testCode/test/testcode.js
+++
@@ -1,5 +0,0 @@
-// test/testcode.js
-// Small helper so all the generated tests that do require('./testcode')
-// get the real implementation from src/math.js
-
-module.exports = require('..');  // this loads src/math.js via package.json "main"
